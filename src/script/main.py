8bf723fdbd7813a45ffb300edce47839a18b8c53
AgafonovSiberia <agafonov@chita.ru>
2023-03-22 13:03:15 +0700

    fixed google api

--- src/script/main.py
+++ src/script/main.py
@@ -1,9 +1,7 @@
 import time
-
 from src.script.services.core import update_record_to_database
 from src.infrastucture.db.factory import create_pool
 from sqlalchemy.orm.session import sessionmaker
-from src.script.services.google_api import get_worksheet
 from src.logger import logger
 
 GOOGLE_API_TIMEOUT = 2
@@ -12,10 +10,9 @@ GOOGLE_API_TIMEOUT = 2
 def script_run():
     logger.info("Start script")
     pool: sessionmaker = create_pool()
-    ws = get_worksheet(0)
 
     while True:
-        update_record_to_database(pool=pool, ws=ws)
+        update_record_to_database(pool=pool)
         time.sleep(GOOGLE_API_TIMEOUT)
 
 
--- src/script/services/core.py
+++ src/script/services/core.py
@@ -1,9 +1,10 @@
 from src.script.schemas import RecordSchema
 from src.script.services.current_rate import get_current_rate_from_api
 from src.infrastucture.repo.record_repo import RecordRepo
-
+from src.script.services.google_api import get_worksheet
 from src.infrastucture.repo.base.repository import get_base_repo
 from sqlalchemy import event
+
 from src.logger import logger
 from src.utils import timeit
 
@@ -11,14 +12,15 @@ WORKSHEET_ID = 0
 
 
 @timeit
-def get_records_from_sheets(ws) -> list[RecordSchema]:
+def get_records_from_sheets() -> list[RecordSchema]:
     """
     Получает все записи из GoogleSheet, приводит к list[object Record]
     :return: list[Record]
-
-    !! Надо обрабатывать ошибки гугла (смотреть лимиты)
     """
+
+    ws = get_worksheet(WORKSHEET_ID)
     values_list = ws.get_all_records()
+
     list_records: list[RecordSchema] = [
         RecordSchema(**record_row) for record_row in values_list
     ]
@@ -26,13 +28,13 @@ def get_records_from_sheets(ws) -> list[RecordSchema]:
 
 
 @timeit
-def update_record_to_database(pool, ws) -> None:
+def update_record_to_database(pool) -> None:
     """
     Обновляет записи из GoogleSheet в БД.
     Считает значение для поля цена в рублях.
     :param pool: Пул соединений с БД
     """
-    list_records: list[RecordSchema] = get_records_from_sheets(ws)
+    list_records: list[RecordSchema] = get_records_from_sheets()
     current_rate = get_current_rate_from_api()
 
     with pool() as _session:
